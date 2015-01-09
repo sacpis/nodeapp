2595ceb5320fd7e291c0dd2b2374c6c06397af8e
sacpis <sacpis@gmail.com>
2015-01-09 15:54:39 -0800

    added index and zipcode scripts

---
+++ mysystem.js
@@ -0,0 +1,17 @@
+var os = require('os')
+
+var message = 'Here is some info about your system';
+
+var sysarray = new Array('Type: ' + os.type(),
+			'Node Version: ' + process.version,
+			'Platform: ' + os.platform(),
+			'Hostname: ' + os.hostname(),
+			'Total Memory: ' + os.totalmem(),
+			'Free Memory: ' + os.freemem(),
+			'Uptime: ' + os.uptime()
+			);
+
+console.log(message);
+for(i=0; i<sysarray.length; i++){
+	console.log(sysarray[i]);
+}
